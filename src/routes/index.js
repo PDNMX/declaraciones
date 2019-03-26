cd4baf6bd21619949f5a13ddbdf6041f00ba7780
Benito Perdomo <benito.perdomo.pdn@gmail.com>
2019-03-26 11:17:28 -0600

    pantalla inicial de la declaracion

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,17 @@
 import Home from "../components/Home";
 import Login from "../components/Login/";
+import Declaracion from "../components/Declaracion/";
 
 const routes = [
   {
     path: "/",
     component: Home,
+    private: true,
+    key: 0
+  },
+  {
+    path: "/declaraciones",
+    component: Declaracion,
     private: true,
     key: 0
   },
