c8c7b8f05189ef27a928151573c0e6edbffe0c86
Benito Perdomo <benito.perdomo.pdn@gmail.com>
2019-04-02 12:06:29 -0600

    pantalla en blanco

--- src/components/Prueba/index.js
+++ src/components/Prueba/index.js
@@ -1,47 +1,30 @@
 import React, { Component } from "react";
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
 const styles = {
   card: {
-    minWidth: 275,
+    minWidth: 275,    
     margin: 10
   },
   title: {
-    fontSize: 14,
+    fontSize: 14
   },
   pos: {
-    marginBottom: 12,
-  },
+    marginBottom: 12
+  }
 };
 
 class SimpleCard extends Component {
-  render(){
+  render() {
+    const { classes } = this.props;
 
-      const { classes } = this.props;
-
-      return (
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography className={classes.title} color="textSecondary" gutterBottom>
-              Title
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              Subtitle
-            </Typography>
-            <Typography component="p">
-              content
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">button</Button>
-          </CardActions>
-        </Card>
-      );
+    return (
+      <Card className={classes.card}>
+        <CardContent />
+      </Card>
+    );
   }
 }
 
